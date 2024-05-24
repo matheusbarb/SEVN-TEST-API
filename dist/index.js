e392d2f0740b2ae6c027094952a891b06f6b29cb
matheusbarb <matheusbarbosacontato00@gmail.com>
2024-05-23 23:31:43 -0300

    feat: add third endpoint for article page and tested with Insomnia

--- dist/index.js
+++ dist/index.js
@@ -32,9 +32,6 @@ var news = [
 app.get("/news", function (req, res) {
     res.json(news);
 });
-app.listen(port, function () {
-    console.log("Servidor rodando em http://localhost:".concat(port));
-});
 //Second Endpoint
 app.get('/news/:id/content', function (req, res) {
     var id = parseInt(req.params.id);
@@ -42,4 +39,16 @@ app.get('/news/:id/content', function (req, res) {
     if (!article)
         return res.status(404).json({ mensagem: 'Notícia não encontrada' });
     res.json({ content: article.content });
+});
+// Third Endpoint
+app.get('/article', function (_, res) {
+    var article = {
+        id: 1,
+        content: 'Banco Central libera consulta a dinheiro esquecido em bancos O Banco Central informou nesta segunda-feira (14) que quem não tiver valores a receber nesta etapa das consultas ao novo site de consulta aos recursos "esquecidos" nos bancos poderá eventualmente ter nas próximas fases do sistema. Muitos clientes que acessam o sistema têm recebido a mensagem para retornar a consulta em 2 de maio (veja na imagem mais abaixo). "Quem não tiver valores a receber nesta etapa poderá ter nas próximas fases", informou o BC ao ser questionado pelo g1. Isso acontece porque, ao todo, os bancos têm R$ 8 bilhões a devolver aos clientes. Mas, nesta primeira fase, foram abertas consultas referentes à metade, R$ 4 bilhões. Banco Central libera consulta de dinheiro "esquecido" em bancos: saiba como fazer Em 2 de maio, as consultas a uma nova fase serão abertas. O BC não informou, no entanto, se todos os R$ 4 bilhões restantes serão liberados para consultas nesta segunda fase. Em janeiro, no entanto, o BC afirmou que todos os recursos seriam liberados "ao longo de 2022". Página do BC informa que cidadão sem valores a receber atualmente poderá fazer nova consulta a partir de maio.',
+        data: '13/02/2022 as 16h30',
+    };
+    res.json(article);
+});
+app.listen(port, function () {
+    console.log("Servidor rodando em http://localhost:".concat(port));
 });
